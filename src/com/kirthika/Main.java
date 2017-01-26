a7683ceb3791c84d2c568c4eddcec347da643f8e
Kirthika S <kirthika05@gmail.com>
2017-01-26 10:12:42 -0600

    Initial commit

---
+++ src/com/kirthika/Main.java
@@ -0,0 +1,48 @@
+package com.kirthika;
+
+import java.util.*;
+
+abstract class Book
+{
+    String title;
+    String author;
+    Book(String t,String a){
+        title=t;
+        author=a;
+    }
+    abstract void display();
+
+
+}
+
+class MyBook extends Book{
+    int price;
+
+    MyBook(String t,String a,int p){
+        super(t,a);
+        price=p;
+    }
+
+    public void display(){
+        System.out.println("Title: "+title);
+        System.out.println("Author: "+author);
+        System.out.println("Price: "+price);
+    }
+
+
+}
+
+public class Main
+{
+
+    public static void main(String []args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String title=sc.nextLine();
+        String author=sc.nextLine();
+        int price=sc.nextInt();
+        Book new_novel=new MyBook(title,author,price);
+        new_novel.display();
+
+    }
+}
